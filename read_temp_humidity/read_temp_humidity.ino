889f290f317e10e921090769a8ded2019d2f8dc8
Robert Stare <robistare@gmail.com>
2023-07-02 20:37:17 +0200

    Add one more hardcoded sensor
    Hardcoding 2 sensors.

--- read_temp_humidity/read_temp_humidity.ino
+++ read_temp_humidity/read_temp_humidity.ino
@@ -1,40 +1,45 @@
 #include <DFRobot_DHT11.h>
-DFRobot_DHT11 DHT;
-#define DHT11_PIN 6
+DFRobot_DHT11 DHT1;
+DFRobot_DHT11 DHT2;
+#define DHT11_PIN_1 6
+#define DHT11_PIN_2 5
 
 #include <LiquidCrystal.h>
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-void setup(){
+void setup() {
   Serial.begin(9500);
-    // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("Hello World!");
 }
 
-void loop(){
-  DHT.read(DHT11_PIN);
+void loop() {
+  DHT1.read(DHT11_PIN_1);
+  DHT2.read(DHT11_PIN_2);
 
-  // Clear the LCD screen
   lcd.clear();
-  
-  Serial.print("temp:");
-  Serial.print(DHT.temperature);
-  Serial.print("  humi:");
-  Serial.println(DHT.humidity);
- 
 
-    // set the cursor to column 0, line 1
-  // (note line 1 is the second row, since counting begins with 0)
+  Serial.print("temp 1: ");
+  Serial.print(DHT1.temperature);
+  Serial.print("  humi 1: ");
+  Serial.println(DHT1.humidity);
+
+  Serial.print("temp 2: ");
+  Serial.print(DHT2.temperature);
+  Serial.print("  humi 2: ");
+  Serial.println(DHT2.humidity);
+
+  float avgTemp = (DHT1.temperature + DHT2.temperature) / 2.0;
+  float avgHumidity = (DHT1.humidity + DHT2.humidity) / 2.0;
+
   lcd.setCursor(0, 0);
-      lcd.print("T:   ");
-      lcd.print(DHT.temperature);
-      lcd.print(" C");
-      lcd.setCursor(0, 1);
-      lcd.print("H:   ");
-      lcd.print(DHT.humidity);
-      lcd.print(" %");
- delay(1000);
+  lcd.print("T: ");
+  lcd.print(avgTemp);
+  lcd.print(" C");
+  lcd.setCursor(0, 1);
+  lcd.print("H: ");
+  lcd.print(avgHumidity);
+  lcd.print(" %");
+
+  delay(1000);
 }
